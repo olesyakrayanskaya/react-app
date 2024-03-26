e7cb4da8142b7931b3cc33d5d0e928580ff338fe
Olesya <krayanskaya_ov@mail.ru>
2024-03-26 19:57:45 +0300

    styles

--- react-app/src/components/Services/Services.jsx
+++ react-app/src/components/Services/Services.jsx
@@ -1,16 +1,25 @@
 import Service from '../Service/Service';
 import { services } from '../../data';
 import classes from './Services.module.css';
+import image from '../../assets/images/Group.svg';
 
 function Services() {
     return (
         <section className={classes.Services}>
             <h2 className={classes.Services__title}>Наши услуги:</h2>
-            <ul className={classes.Services__list}>
-                {services.map((s) => (
-                    <Service key={s.title} {...s} />
-                ))}
-            </ul>
+            <div className={classes.Services__content}>
+                <ul className={classes.Services__list}>
+                    {services.map((s) => (
+                        <Service key={s.title} {...s} />
+                    ))}
+                </ul>
+                <img
+                    className={classes.Services__img}
+                    src={image}
+                    width={400}
+                    aria-hidden="true"
+                ></img>
+            </div>
         </section>
     );
 }
